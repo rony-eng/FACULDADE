7052f3d5984e489ba6e2450166089e6d714746d5
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-08-25 16:47:01 -0300

    PDO PHP

---
+++ HTML, CSS, JAVASCRIPT E PHP/PHP/PDO.php
@@ -0,0 +1,29 @@
+<?php
+
+//Constantes para armazenamento das variáveis de conexão.
+define('HOST', '127.0.0.1');
+define('PORT', '5432');
+define('DBNAME', 'test');
+define('USER', 'user');
+define('PASSWORD', 'psswd');
+
+try {
+	$dsn = new PDO("mysql:host=". HOST . ";port=".PORT.";dbname=" . DBNAME .";user=" . USER . ";password=" . PASSWORD);
+} catch (PDOException $e) {
+	echo 'A conexão falhou e retorno a seguinte mensagem de erro: ' .$e->getMessage();
+}
+
+//Realizando uma consulta no BD através do login e senha recebidos por POST
+$login = $_POST['login'];
+$pswd = $_POST['pswd'];
+
+//métos prepare e execute para prevenção de sqlinjection
+$stmt = $dsn->prepare("Select * From Usuario Where login = :login And password =:password");
+$stmt->execute([':login' => $login, ':password' => $pswd]);
+
+/*Aqui entraria o código para tratar o resultado da instrução SQL acima*/
+
+
+//Destruindo o objecto statement e fechando a conexão
+$stmt = null;
+$dsn = null;
