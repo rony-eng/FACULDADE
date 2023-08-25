a19d35398151c77141276e93772b40f4821e4d94
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-08-25 16:46:27 -0300

    Operadores PHP

---
+++ HTML, CSS, JAVASCRIPT E PHP/PHP/operadores.php
@@ -0,0 +1,42 @@
+<?php
+
+//operadores aritméticos
+
+$var1 = 11;
+$var2 = 2
+
+$modulo = $var1 % $var2;
+//retorna o resto da divisão
+echo "Resultado do modulo: " .
+$modulo;
+
+echo "\n\n";
+die;
+
+$var1 = 10;
+$var2 = 2;
+$exponencial = $var1 ** $var2;
+//$var1 elevada a $var2
+echo "Resultado do exponencial: " . $exponencial;
+
+// operador de atribuições
+
+$var4 = 5;
+
+echo "1. var4: $var4";
+
+echo "\n\n";
+
+$var4 += 15; // adiciona o 15 com os 5 que já estava
+echo "2. var4: $var4";
+die;
+echo "\n\n";
+
+$var5 = 50;
+$var5 *= 10; //multiplica por 10
+echo "var5: $var5";
+
+echo "\n\n";
+die;
+
+?>
