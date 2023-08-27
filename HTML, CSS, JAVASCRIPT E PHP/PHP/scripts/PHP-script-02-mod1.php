91eb26c97ce0e03c03918aa4c4b533c65b2a070e
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-08-27 12:39:01 -0300

    PHP script 02 mod1

---
+++ HTML, CSS, JAVASCRIPT E PHP/PHP/scripts/PHP-script-02-mod1.php
@@ -0,0 +1,13 @@
+<?php
+// Comentários de uma linha são realizados com a utilização de barras duplas
+
+/*
+Cada comando deve ser terminado com ; 
+Abaixo temos alguns exemplos de término de comandos
+*/
+
+echo "Olá, mundo";
+
+$var1 = 2 + 2;
+
+echo "O Resultado da soma é igual a: " . Svar1;
