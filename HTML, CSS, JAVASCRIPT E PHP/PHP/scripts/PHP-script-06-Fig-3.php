b1f66d0a3bc1a5b2faf70ccab8f82fcf27c3a583
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-08-29 10:02:37 -0300

    PHP script 06 fig 3

---
+++ HTML, CSS, JAVASCRIPT E PHP/PHP/scripts/PHP-script-06-Fig-3.php
@@ -0,0 +1,8 @@
+<?php
+
+    //Requisição GET:
+    /endereco_servido/script.php?var1=value1&var2=value2=var3=value3
+
+    echo $_GET['var1']; //imprimiria value1
+    echo $_GET['var2']; //imprimiria value2
+    echo $_GET['var3']; //imprimiria value3
