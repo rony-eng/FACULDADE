62a6a4aca811dc5f01c99c0430bd3738b99e8c3a
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-09-05 09:42:51 -0300

    PHP script 15 mod2

---
+++ HTML, CSS, JAVASCRIPT E PHP/PHP/scripts/PHP-script-15-mod2.php
@@ -0,0 +1,16 @@
+<?php
+
+$i = 2;
+while ($i <= 20){
+    echo $i;
+    $i += 2;
+    echo "\n";
+}
+
+/* Sintaxe alternativa */
+$i = 2;
+while ($i <= 20):
+    echo $i;
+    $i += 2;
+    echo "\n";
+endwhile
