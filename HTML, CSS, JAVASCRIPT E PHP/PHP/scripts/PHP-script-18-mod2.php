376ac1a4cb7f01776c54ac8041be4f834d9bac88
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-09-07 13:40:57 -0300

    PHP script 18 mod2

---
+++ HTML, CSS, JAVASCRIPT E PHP/PHP/scripts/PHP-script-18-mod2.php
@@ -0,0 +1,12 @@
+<?php
+$carros = Array("Fusca", "Monza", "Passat");
+
+foreach($carros as $carro){
+    echo $carro
+    echo "\n"
+}
+
+for ($i = 0; $i < count($carros); $i++){
+    echo $carros[$i];
+    echo "\n";
+}
