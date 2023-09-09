d186217cf63711c8cb7218f5bc1abc44b4249989
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-09-09 14:27:00 -0300

    PHP script 22 mod3

---
+++ HTML, CSS, JAVASCRIPT E PHP/PHP/scripts/PHP-script-22-mod3.php
@@ -0,0 +1,21 @@
+<?php
+$carros = array(
+    'vw'        => "Fusca",
+    0           => "Passat",
+    'chevrolet' => "Monza",
+    1           => "Chevette",
+    'fiat'      => "Tempra",
+    2           => "Uno"
+);
+
+//A impressão desse array gera a seguinte saída
+//Faça você mesmo: utilize a função print_r($carros); e veja o resultado em sua tela.
+
+Array(
+    [vw]        => Fusca
+    [0]         => Passat 
+    [chevrolet] => Monza 
+    [1]         => Chevet 
+    [fiat]      => Tempra 
+    [2]         => Uno 
+)
