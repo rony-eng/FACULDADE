03b3d711a37952b041b5d939c807aa9e664398ee
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-09-10 12:20:52 -0300

    PHP script 24 mod3

---
+++ HTML, CSS, JAVASCRIPT E PHP/PHP/scripts/PHP-script-24-mod3.php
@@ -0,0 +1,18 @@
+<?php
+Array
+(
+    (vermelhas) => Array
+        (
+            [0] => melancia
+            [1] => cereja
+            [2] => framboesa
+            [3] => morango
+        )
+    (citricas) => Array
+        (
+            [0] => laranja
+            [1] => limão 
+            [2] => abacaxi
+            [3] => mexerica
+        )
+)
