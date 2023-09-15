51515d642079373bb19795c143a378f7dfc6269b
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-09-15 10:56:36 -0300

    PHP PDO Script 06 Mod2

---
+++ HTML, CSS, JAVASCRIPT E PHP/PHP/Scripts_Tema_PHP_PDO/PHP-PDO-Script-06-Mod2.php
@@ -0,0 +1,10 @@
+<?php 
+include "fruta.class.php";
+ 
+$fruta1 = new Fruta();
+$fruta1->setNome("morango");
+$fruta1->setTipo("vermelha");
+ 
+$fruta2 = new Fruta();
+$fruta2->setNome("laranja");
+$fruta2->setTipo("cítrica");
